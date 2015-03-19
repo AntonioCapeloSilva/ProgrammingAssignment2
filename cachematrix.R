b8fca2f53d71fe06721ca7c476b08c445984c20d
AntonioCapeloSilva <antoniocapelobarroso@gmail.com>
2015-03-19 16:21:46 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix cache the matrix and the solve
 
-## Write a short comment describing this function
+## Cache the matrix and the inverted matrix, show the enviroment where both values are
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix(),...) {
+    cache1 <- x
+    getfunction <- function() cache1
+    setfunction <- function(y = NULL) {
+      cache1 <<- y 
+    }
+    #Solve the matrix and put it on cache aswell
+    m<<- solve(x)
+    #Show environment that x is in cache
+    list(get = getfunction, set = setfunction)
+ 
+  }
 
-}
 
-
-## Write a short comment describing this function
+## See if values are already assigned, if not run makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # See if there's a assigned value on cache      
+  if(x != NULL){
+          message("Matrix assigned in Cache")
+          #See if the inverted matrix is in cache
+          if( m = NULL){
+            message("No inverted Matrix, make sure that the original matrix is quadratic")
+          }
+        }
+  #If there is no value assigned, run makeCacheMatrix
+        else{
+          matrixCacheMatrix(x)
+        }
 }
